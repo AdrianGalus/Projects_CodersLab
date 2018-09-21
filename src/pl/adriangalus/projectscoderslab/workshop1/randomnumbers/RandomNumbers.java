c9ba38b163cb1a125263cf1c7e366d0ce69da0d2
AdrianGalus <adriangalus8@gmail.com>
2018-09-21 13:54:17 +0200

    RandomNumbers

--- src/pl/adriangalus/projectscoderslab/workshop1/randomnumbers/RandomNumbers.java
+++ src/pl/adriangalus/projectscoderslab/workshop1/randomnumbers/RandomNumbers.java
@@ -1,8 +1,39 @@
 package pl.adriangalus.projectscoderslab.workshop1.randomnumbers;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class RandomNumbers {
 
     public static void main(String[] args) {
-	
+
+        int randomNumber = random(100);
+
+        System.out.println("Zgadnij liczbę (od 1 do 100). Jeżeli chcesz się poddać, wybierz 0");
+        int userNumber;
+        while((userNumber = getData()) != 0) {
+            if (userNumber < randomNumber) {
+                System.out.println("Za mało!");
+            } else if (userNumber > randomNumber) {
+                System.out.println("Za dużo!");
+            } else {
+                System.out.println("Zgadłeś!");
+                break;
+            }
+        }
+    }
+    private static int random(int size) {
+
+        Random generator = new Random();
+        return generator.nextInt(size)+1;
+    }
+    private static int getData() {
+        Scanner scanner = new Scanner(System.in);
+        scanner.useDelimiter("\n");
+        while(!scanner.hasNextInt()) {
+            scanner.nextLine();
+            System.err.println("To nie jest liczba całkowita");
+        }
+        return scanner.nextInt();
     }
 }
