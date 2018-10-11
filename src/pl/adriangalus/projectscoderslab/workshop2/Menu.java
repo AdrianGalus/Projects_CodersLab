ff7b6c39fc355197a1939bb602f2eaad06bc415e
AdrianGalus <adriangalus8@gmail.com>
2018-10-11 17:00:17 +0200

    add Menu class

---
+++ src/pl/adriangalus/projectscoderslab/workshop2/Menu.java
@@ -0,0 +1,12 @@
+package pl.adriangalus.projectscoderslab.workshop2;
+
+public class Menu {
+
+    static void showMenu(String text) {
+
+        System.out.println(String.format("dodanie %s -> add", text));
+        System.out.println(String.format("edycja %s -> edit", text));
+        System.out.println(String.format("usunięcie $s -> delete", text));
+        System.out.println("zakończenie programu -> quit");
+    }
+}
