f57d2506b9d037ccdd96e952f93cb6b5d4ad46bf
AdrianGalus <adriangalus8@gmail.com>
2018-10-11 16:42:18 +0200

    add ArgumentReader class

---
+++ src/pl/adriangalus/projectscoderslab/workshop2/ArgumentReader.java
@@ -0,0 +1,32 @@
+package pl.adriangalus.projectscoderslab.workshop2;
+
+import java.util.Scanner;
+
+class ArgumentReader {
+
+    static final String ID_PATTERN = "[1-9]+[0-9]*";
+    static final String NAME_PATTERN = "[A-Za-z0-9]+";
+    static final String EMAIL_PATTERN =
+                    "[_a-zA-Z0-9-]+(\\.[_a-zA-Z0-9-]+)*@[a-zA-Z0-9-]+(\\.[a-zA-Z0-9-]+)*\\.([a-zA-Z]{2,}){1}";
+    static final String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$";
+
+    static int getNumber(Scanner scanner, String pattern, String text) {
+
+        String maybeNumber = "";
+        while (!maybeNumber.matches(pattern)) {
+            System.out.println(String.format("Podaj %s", text));
+            maybeNumber = scanner.nextLine();
+        }
+        return Integer.parseInt(maybeNumber);
+    }
+
+    static String getString(Scanner scanner, String pattern, String text) {
+
+        String maybeString = "";
+        while (!maybeString.matches(pattern)) {
+            System.out.println(String.format("Podaj %s", text));
+            maybeString = scanner.nextLine();
+        }
+        return maybeString;
+    }
+}
