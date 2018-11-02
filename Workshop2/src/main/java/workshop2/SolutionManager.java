9365fdd2ebff92d7a63fe288253fc3144030a4f4
AdrianGalus <adriangalus8@gmail.com>
2018-11-02 11:39:13 +0100

    correct switch

--- Workshop2/src/main/java/workshop2/SolutionManager.java
+++ Workshop2/src/main/java/workshop2/SolutionManager.java
@@ -18,46 +18,41 @@ public class SolutionManager {
             int userIdInDB;
             int exerciseIdInDB;
             String choice = scanner.nextLine();
-            switch (choice) {
-                case "add":
-                    Exercise[] exercises = new Exercise[0];
-                    users = UserManager.showUsers(users);
-                    usersId = ArgumentReader.getIndex(scanner, ArgumentReader.ID_PATTERN, "id", users);
-                    userIdInDB = users[usersId-1].getId();
-                    exercises = ExerciseManager.showExercises(exercises);
-                    exerciseId = ArgumentReader.getIndex(scanner, ArgumentReader.ID_PATTERN, "id zadania", exercises);
-                    exerciseIdInDB = exercises[exerciseId-1].getId();
-                    Solution solution = new Solution(exerciseIdInDB, userIdInDB);
-                    try {
-                        Connection conn = ConnectDB.getConnection("programming_school", "root", "coderslab");
+            Connection conn;
+            try {
+                switch (choice) {
+                    case "add":
+                        Exercise[] exercises = new Exercise[0];
+                        users = UserManager.showUsers(users);
+                        usersId = ArgumentReader.getIndex(scanner, ArgumentReader.ID_PATTERN, "id", users);
+                        userIdInDB = users[usersId - 1].getId();
+                        exercises = ExerciseManager.showExercises(exercises);
+                        exerciseId = ArgumentReader.getIndex(scanner, ArgumentReader.ID_PATTERN, "id zadania", exercises);
+                        exerciseIdInDB = exercises[exerciseId - 1].getId();
+                        Solution solution = new Solution(exerciseIdInDB, userIdInDB);
+                        conn = ConnectDB.getConnection("programming_school", "root", "coderslab");
                         solution.saveToDB(conn);
-                    }
-                    catch (SQLException e) {
-                        e.printStackTrace();
-                    }
-                    break;
-                case "view":
-                    Solution[] solutions = new Solution[0];
-                    users = UserManager.showUsers(users);
-                    usersId = ArgumentReader.getIndex(scanner, ArgumentReader.ID_PATTERN, "id", users);
-                    userIdInDB = users[usersId-1].getId();
-                    try {
-                        Connection conn = ConnectDB.getConnection("programming_school", "root", "coderslab");
-                        solutions = Solution.loadAllByUserId(conn, userIdInDB);
-                    }
-                    catch (SQLException e) {
-                        e.printStackTrace();
-                    }
-                    System.out.println("Id\tCreated\tDescription");
-                    int index = 1;
-                    for (Solution sol : solutions) {
-                        System.out.println(index++ + "\t" + sol.getCreated() + "\t" + sol.getDescription());
-                    }
-                    break;
-                case "quit":
-                    System.out.println("Nie będziesz tęsknić?");
-                    active = false;
-                    break;
+                        break;
+                    case "view":
+                        users = UserManager.showUsers(users);
+                        usersId = ArgumentReader.getIndex(scanner, ArgumentReader.ID_PATTERN, "id", users);
+                        userIdInDB = users[usersId - 1].getId();
+                        conn = ConnectDB.getConnection("programming_school", "root", "coderslab");
+                        Solution[] solutions = Solution.loadAllByUserId(conn, userIdInDB);
+                        System.out.println("Id\tCreated\tDescription");
+                        int index = 1;
+                        for (Solution sol : solutions) {
+                            System.out.println(index++ + "\t" + sol.getCreated() + "\t" + sol.getDescription());
+                        }
+                        break;
+                    case "quit":
+                        System.out.println("Nie będziesz tęsknić?");
+                        active = false;
+                        break;
+                }
+            }
+            catch(SQLException e) {
+                e.printStackTrace();
             }
         }
     }
