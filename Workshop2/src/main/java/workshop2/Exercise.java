de5152bc0dc960b13dfa19d30dfbff1e016e5199
AdrianGalus <adriangalus8@gmail.com>
2018-11-02 17:38:36 +0100

    add setId method

--- Workshop2/src/main/java/workshop2/Exercise.java
+++ Workshop2/src/main/java/workshop2/Exercise.java
@@ -24,6 +24,10 @@ public class Exercise {
 
         return id;
     }
+    public void setId(int id) {
+
+        this.id = id;
+    }
     public void setTitle(String title) {
 
         this.title = title;
