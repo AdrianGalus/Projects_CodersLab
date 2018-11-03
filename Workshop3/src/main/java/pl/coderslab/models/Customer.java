12d004fabf29af0051471b1464e8c539e17f6478
AdrianGalus <adriangalus8@gmail.com>
2018-11-03 13:25:12 +0100

    fill Customer class - add constructors, getters and setters

--- Workshop3/src/main/java/pl/coderslab/models/Customer.java
+++ Workshop3/src/main/java/pl/coderslab/models/Customer.java
@@ -1,4 +1,43 @@
 package pl.coderslab.models;
 
+import java.time.LocalDate;
+
 public class Customer {
+
+    private String name;
+    private String surname;
+    private LocalDate birthDate;
+
+    public Customer() {}
+
+    public Customer(String name, String surname, LocalDate birthDate) {
+
+        this.name = name;
+        this.surname = surname;
+        this.birthDate = birthDate;
+    }
+    public String getName() {
+
+        return name;
+    }
+    public void setName(String name) {
+
+        this.name = name;
+    }
+    public String getSurname() {
+
+        return surname;
+    }
+    public void setSurname(String surname) {
+
+        this.surname = surname;
+    }
+    public LocalDate getBirthDate() {
+
+        return birthDate;
+    }
+    public void setBirthDate(LocalDate birthDate) {
+
+        this.birthDate = birthDate;
+    }
 }
