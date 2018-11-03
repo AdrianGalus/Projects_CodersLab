6e110478b6fb5d9b8236517d5c98fa0f7d39652f
AdrianGalus <adriangalus8@gmail.com>
2018-11-03 13:21:07 +0100

    fill Employee class - add contructors, getters and setters

--- Workshop3/src/main/java/pl/coderslab/models/Employee.java
+++ Workshop3/src/main/java/pl/coderslab/models/Employee.java
@@ -1,4 +1,70 @@
 package pl.coderslab.models;
 
 public class Employee {
+
+    private String name;
+    private String surname;
+    private String address;
+    private String phone;
+    private String note;
+    private double costPerHour;
+
+    public Employee() {}
+
+    public Employee(String name, String surname, String address, String phone, String note, double costPerHour) {
+
+        this.name = name;
+        this.surname = surname;
+        this.address = address;
+        this.phone = phone;
+        this.note = note;
+        this.costPerHour = costPerHour;
+    }
+    public String getName() {
+
+        return name;
+    }
+    public void setName(String name) {
+
+        this.name = name;
+    }
+    public String getSurname() {
+
+        return surname;
+    }
+    public void setSurname(String surname) {
+
+        this.surname = surname;
+    }
+    public String getAddress() {
+
+        return address;
+    }
+    public void setAddress(String address) {
+
+        this.address = address;
+    }
+    public String getPhone() {
+
+        return phone;
+    }
+    public void setPhone(String phone) {
+
+        this.phone = phone;
+    }
+    public String getNote() {
+
+        return note;
+    }
+    public void setNote(String note) {
+        this.note = note;
+    }
+    public double getCostPerHour() {
+
+        return costPerHour;
+    }
+    public void setCostPerHour(double costPerHour) {
+
+        this.costPerHour = costPerHour;
+    }
 }
