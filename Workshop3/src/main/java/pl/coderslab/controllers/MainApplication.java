ffd314702f4476512565e56f2aa410ee10822fad
AdrianGalus <adriangalus8@gmail.com>
2018-11-04 17:06:27 +0100

    add doGet method

--- Workshop3/src/main/java/pl/coderslab/controllers/MainApplication.java
+++ Workshop3/src/main/java/pl/coderslab/controllers/MainApplication.java
@@ -1,11 +1,18 @@
 package pl.coderslab.controllers;
 
+import pl.coderslab.dao.OrderDao;
+import pl.coderslab.models.Order;
+import pl.coderslab.utils.DbUtil;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
 
 @WebServlet(name = "MainApplication", urlPatterns = {"/main"})
 public class MainApplication extends HttpServlet {
@@ -15,6 +22,15 @@ public class MainApplication extends HttpServlet {
     }
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        OrderDao orderDao = new OrderDao();
+        try {
+            Connection conn = DbUtil.getConn();
+            List<Order> orders = orderDao.loadAllOrders(conn);
+            request.setAttribute("orders", orders);
+        }
+        catch(SQLException e) {
+            e.printStackTrace();
+        }
         getServletContext().getRequestDispatcher("/index.jsp").forward(request, response);
     }
 }
