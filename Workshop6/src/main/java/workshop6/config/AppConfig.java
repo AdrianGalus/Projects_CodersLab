25c27a3bc706290d9cbcd27c0f5fecac2c757d01
AdrianGalus <adriangalus8@gmail.com>
2018-11-10 20:14:12 +0100

    add Validator bean

--- Workshop6/src/main/java/workshop6/config/AppConfig.java
+++ Workshop6/src/main/java/workshop6/config/AppConfig.java
@@ -14,6 +14,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import javax.persistence.EntityManagerFactory;
+import javax.validation.Validator;
 
 @Configuration
 @EnableWebMvc
@@ -47,5 +48,10 @@ public class AppConfig extends WebMvcConfigurerAdapter {
 
         JpaTransactionManager tm = new JpaTransactionManager(emf);
         return tm;
+    }
+    @Bean
+    public Validator validator() {
+
+        return new LocalValidatorFactoryBean();
     }
 }
