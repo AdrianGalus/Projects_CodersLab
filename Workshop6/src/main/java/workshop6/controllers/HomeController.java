06c6deeca8aa1de4302d586d32fea72abb3d3580
AdrianGalus <adriangalus8@gmail.com>
2018-11-10 20:14:38 +0100

    correct link

--- Workshop6/src/main/java/workshop6/controllers/HomeController.java
+++ Workshop6/src/main/java/workshop6/controllers/HomeController.java
@@ -2,8 +2,10 @@ package workshop6.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/home")
 public class HomeController {
 
     @GetMapping("")
