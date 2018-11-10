0b5363c3dcfe74ac0085797c029ee087312f1747
AdrianGalus <adriangalus8@gmail.com>
2018-11-10 19:16:00 +0100

    add UserRepository

---
+++ Workshop6/src/main/java/workshop6/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package workshop6.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import workshop6.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    
+}
