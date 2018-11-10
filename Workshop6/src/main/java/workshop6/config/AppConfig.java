7abde90b32382ed7763a2feb1e6760338f9e2cc9
AdrianGalus <adriangalus8@gmail.com>
2018-11-10 20:33:46 +0100

    remove unnecessary imports

--- Workshop6/src/main/java/workshop6/config/AppConfig.java
+++ Workshop6/src/main/java/workshop6/config/AppConfig.java
@@ -7,14 +7,12 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import javax.persistence.EntityManagerFactory;
-import javax.validation.Validator;
 
 @Configuration
 @EnableWebMvc
