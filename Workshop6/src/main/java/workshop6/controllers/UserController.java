15f7d5b0b81ab1342dfb77f11cb511984d1d7732
AdrianGalus <adriangalus8@gmail.com>
2018-11-10 19:44:57 +0100

    add registration in post

--- Workshop6/src/main/java/workshop6/controllers/UserController.java
+++ Workshop6/src/main/java/workshop6/controllers/UserController.java
@@ -1,17 +1,37 @@
 package workshop6.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import workshop6.entity.User;
+import workshop6.repository.UserRepository;
+
+import javax.validation.Valid;
 
 @Controller
 public class UserController {
+
+    @Autowired
+    UserRepository userRepository;
 
     @GetMapping("/registration")
     public String registration(Model model) {
 
         model.addAttribute("user", new User());
         return "registration";
+    }
+    @PostMapping("/registration")
+    public String registration(@Valid User user, BindingResult result) {
+
+        if(result.hasErrors()) {
+            return "registration";
+        }
+        else {
+            userRepository.save(user);
+            return "redirect:/";
+        }
     }
 }
