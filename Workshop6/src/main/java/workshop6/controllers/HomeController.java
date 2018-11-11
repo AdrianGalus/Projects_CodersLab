d2d8815db6c6b65145291d71a7c9a9dbd0bb8c1d
AdrianGalus <adriangalus8@gmail.com>
2018-11-11 16:05:22 +0100

    modify home method

--- Workshop6/src/main/java/workshop6/controllers/HomeController.java
+++ Workshop6/src/main/java/workshop6/controllers/HomeController.java
@@ -1,16 +1,23 @@
 package workshop6.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import workshop6.repository.TweetRepository;
 
 @Controller
 @RequestMapping("/home")
 public class HomeController {
 
-    @GetMapping("")
-    public String home() {
+    @Autowired
+    TweetRepository tweetRepository;
 
+    @GetMapping("")
+    public String home(Model model) {
+
+        model.addAttribute("tweets", tweetRepository.findAll());
         return "index";
     }
 }
