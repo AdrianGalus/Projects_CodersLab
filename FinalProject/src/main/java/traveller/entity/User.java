b15b2a4c1ad114887310efcb1315d0190c385892
AdrianGalus <adriangalus8@gmail.com>
2018-11-12 23:34:24 +0100

    make entity

--- FinalProject/src/main/java/traveller/entity/User.java
+++ FinalProject/src/main/java/traveller/entity/User.java
@@ -1,17 +1,36 @@
 package traveller.entity;
 
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotBlank;
 import org.mindrot.jbcrypt.BCrypt;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "users")
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotNull
+    @NotBlank
+    private String userName;
+    @NotNull
+    @NotBlank
+    @Email
+    @Column(unique = true)
     private String email;
+    @NotNull
+    @NotBlank
     private String password;
 
     public User() {}
 
-    public User(String email, String password) {
+    public User(String userName, String email, String password) {
 
+        this.userName = userName;
         this.email = email;
         this.setPassword(password);
     }
@@ -24,6 +43,14 @@ public class User {
         if (this.id == null) {
             this.id = id;
         }
+    }
+    public String getUserName() {
+
+        return userName;
+    }
+    public void setUserName(String userName) {
+
+        this.userName = userName;
     }
     public String getEmail() {
 
