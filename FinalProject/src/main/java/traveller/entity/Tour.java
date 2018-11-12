0b5b6c56695ab9d6035543663267dd911ccf505f
AdrianGalus <adriangalus8@gmail.com>
2018-11-12 23:13:16 +0100

    add drivers

--- FinalProject/src/main/java/traveller/entity/Tour.java
+++ FinalProject/src/main/java/traveller/entity/Tour.java
@@ -9,6 +9,8 @@ public class Tour {
     private Long id;
     private Customer customer;
     private Coach coach;
+    private Driver driver1;
+    private Driver driver2;
     private LocalDate orderDate;
     private LocalDateTime departureDate;
     private LocalDateTime arrivalDate;
@@ -19,11 +21,14 @@ public class Tour {
 
     public Tour() {}
 
-    public Tour(Customer customer, Coach coach, LocalDate orderDate, LocalDateTime departureDate, LocalDateTime arrivalDate,
-                String destination, BigDecimal distance, BigDecimal cost, Boolean paid) {
+    public Tour(Customer customer, Coach coach, Driver driver1, Driver driver2, LocalDate orderDate,
+                LocalDateTime departureDate, LocalDateTime arrivalDate, String destination, BigDecimal distance,
+                BigDecimal cost, Boolean paid) {
 
         this.customer = customer;
         this.coach = coach;
+        this.driver1 = driver1;
+        this.setDriver2(driver2);
         this.orderDate = orderDate;
         this.departureDate = departureDate;
         this.arrivalDate = arrivalDate;
@@ -57,6 +62,27 @@ public class Tour {
     public void setCoach(Coach coach) {
 
         this.coach = coach;
+    }
+    public Driver getDriver1() {
+
+        return driver1;
+    }
+    public void setDriver1(Driver driver1) {
+
+        this.driver1 = driver1;
+    }
+    public Driver getDriver2() {
+
+        return driver2;
+    }
+    public void setDriver2(Driver driver2) {
+
+        if(this.driver1 != null) {
+            this.driver2 = driver2;
+        }
+        else {
+            this.driver1 = driver2;
+        }
     }
     public LocalDate getOrderDate() {
 
